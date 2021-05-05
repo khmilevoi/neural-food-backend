8b16784e2b3d860a8b812de360946c5a63dd9da4
khmilevoi <khmilevoi@gmail.com>
2021-05-05 18:18:38 +0300

    add actions

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv'
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
-import { readFileSync } from "fs";
-import https from "https";
+import http from "http";
 import {generateLabelsFromFile} from "labels";
+
 
 dotenv.config({
     path: "./.env"
@@ -11,8 +11,8 @@ dotenv.config({
 
 const isLabels = process.argv[2] === "labels";
 
-if(isLabels) {
-    generateLabelsFromFile().then()
+if (isLabels) {
+    generateLabelsFromFile().then();
 }
 
 export const app = express();
@@ -22,14 +22,8 @@ app.use(cors());
 app.use("/model", express.static("resources/model.json"));
 app.use("/labels", express.static("resources/labels.json"));
 
-export const server = https
-  .createServer(
-    {
-      key: readFileSync("./security/key.pem", "utf8"),
-      cert: readFileSync("./security/server.crt", "utf8"),
-    },
-    app
-  )
-  .listen(3000, () => {
-    console.log("Started on 3000 port");
-  });
+export const server = http
+    .createServer(app)
+    .listen(3000, () => {
+        console.log("Started on 3000 port");
+    });
